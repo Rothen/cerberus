9ac56e9d30eb04b65f56f0c926e012dbab98355c
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2022-05-31 13:07:41 +0200

    update

--- tests/open_door.py
+++ tests/open_door.py
@@ -10,6 +10,5 @@ OPEN_VOICE_CHANNEL = 0x309E8100
 CONTROL_SEQUENCE = 0x5802
 
 ser = serial.Serial("/dev/ttyS0", 9600)
-# ser.write(bytes('%s\r\n' % ('GOT: 0x109e8141, CRC: 0, CALC_CRC: 0'), 'utf-8'))
 ser.write(bytes('%s\r\n' % (hex(OPEN_DOOR)), 'utf-8'))
 ser.close()
---
+++ tests/test_uart_worker.py
@@ -0,0 +1,13 @@
+import serial
+
+RING_UPSTAIRS = 0x109E8141
+RING_DOWNSTAIRS = 0x009E8180
+CANCEL_VOICE = 0x3080
+CANCEL_ACTIVITY = 0x2480
+OPEN_DOOR = 0x109E8180
+OPEN_VOICE_CHANNEL = 0x309E8100
+CONTROL_SEQUENCE = 0x5802
+
+ser = serial.Serial("/dev/ttyS0", 9600)
+ser.write(bytes('GOT: %s, CRC: 0, CALC_CRC: 0\r\n' % (hex(RING_UPSTAIRS)), 'utf-8'))
+ser.close()
