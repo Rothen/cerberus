e6980904bf38c77428ad2178617b16f06913a527
Benjamin Ricchiuto <beni.ricchiuto@gmail.com>
2023-01-15 08:28:56 +0000

    update

--- esphome/cerberus.h
+++ esphome/cerberus.h
@@ -1,6 +1,8 @@
-#include "esphome.h"
 #include "gpio.h"
+#include "esphome.h"
 #include "tcs_bus.h"
+
+#define getCerberus(constructor) static_cast<Cerberus *>((&constructor)->get_component(0))
 
 #define RING_UPSTAIRS 0x109E8141
 #define RING_DOWNSTAIRS 0x009E8180 // 0x009E8181 0x009E8182
@@ -11,7 +13,7 @@
 #define OPEN_VOICE_CHANNEL 0x309E8100
 #define CONTROL_SEQUENCE 0x5802
 
-class Cerberus : public Component, public CustomAPIDevice
+class Cerberus : public Component
 {
     public:
         TCSBusReader tcsReader = TCSBusReader(D2);
@@ -21,18 +23,38 @@ class Cerberus : public Component, public CustomAPIDevice
         byte s_curCRC = 0;
         byte s_calCRC = 0;
 
+        BinarySensor *ringingUpstairsSensor = new BinarySensor();
+        BinarySensor *ringingDownstairsSensor = new BinarySensor();
+        bool ringingUpstairs = false;
+        bool ringingDownstairs = false;
+
+        unsigned long previousMillis = 0UL;
+        unsigned long interval = 1000UL;
+
         void setup() override
         {
             tcsWriter.begin();
             tcsReader.begin();
 
-            register_service(&Cerberus::onOpenDoor, "open_door");
-            register_service(&Cerberus::onStartWasherCycle, "start_washer_cycle", {"cycle_duration", "silent", "string_argument"});
-            subscribe_homeassistant_state(&Cerberus::onStateChanged, "sensor.temperature");
+            resetRingingSensors();
         }
 
         void loop() override
         {
+            if (ringingUpstairs or ringingDownstairs) {
+                unsigned long currentMillis = millis();
+
+                if(currentMillis - previousMillis > interval)
+                {
+                    ESP_LOGD("read command", "Delay expired");
+                    resetRingingSensors();
+
+                    previousMillis = currentMillis;
+                }
+            } else {
+                previousMillis = 0;
+            }
+
             if (tcsReader.hasCommand())
             {
                 tcsReader.read(&s_cmd, &s_curCRC, &s_calCRC);
@@ -61,29 +83,14 @@ class Cerberus : public Component, public CustomAPIDevice
                     ESP_LOGD("read command", "Unknown command recieved %d (CRC: %d, Calc CRC: %d)", s_cmd, s_curCRC, s_calCRC);
                 }
             }
-
-            if (is_connected()) {
-                // Example check to see if a client is connected
-            }
         }
+
         void onOpenDoor()
         {
-            ESP_LOGD("custom", "Hello World!");
-
-            if (is_connected()) {
-            // Example check to see if a client is connected
-            }
-        }
-        void onStartWasherCycle(int cycle_duration, bool silent, std::string string_argument)
-        {
-            ESP_LOGD("custom", "Starting washer cycle!");
+            ESP_LOGD("cerberus", "Opening door");
             tcsReader.disable();
             tcsWriter.write(OPEN_DOOR);
             tcsReader.enable();
-            // do something with arguments
-
-            // Call a homeassistant service
-            call_homeassistant_service("homeassistant.service");
         }
 
         void onStateChanged(std::string state)
@@ -91,14 +98,34 @@ class Cerberus : public Component, public CustomAPIDevice
             ESP_LOGD("state changed", "Temperature has changed to %s", state.c_str());
         }
 
+        void setRingingUpstairs(bool ringingUpstairs)
+        {
+            this->ringingUpstairs = ringingUpstairs;
+            ringingUpstairsSensor->publish_state(ringingUpstairs);
+        }
+
+        void setRingingDownstairs(bool ringingDownstairs)
+        {
+            this->ringingDownstairs = ringingDownstairs;
+            ringingDownstairsSensor->publish_state(ringingDownstairs);
+        }
+
         void onRingUpstairs()
         {
             ESP_LOGD("read command", "Recieved RING_UPSTAIRS command (CRC: %d, Calc CRC: %d)", s_curCRC, s_calCRC);
+            setRingingUpstairs(true);
         }
 
         void onRingDownstairs()
         {
             ESP_LOGD("read command", "Recieved RING_DOWNSTAIRS command (CRC: %d, Calc CRC: %d)", s_curCRC, s_calCRC);
+            setRingingDownstairs(true);
+        }
+
+        void resetRingingSensors()
+        {
+            setRingingUpstairs(false);
+            setRingingDownstairs(false);
         }
 
         void onCancelVoiceControlSequence()
